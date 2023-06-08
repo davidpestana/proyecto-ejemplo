2a645180de2f97a205b3b968cf5e7c91b932b59f
davidpestana <david.pestana.perdomo@gmail.com>
2023-06-08 10:48:50 +0200

    kafka example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const { Kafka } = require('kafkajs');
+
+const kafka = new Kafka({
+  brokers: [process.env.KAFKA_HOST || 'localhost:9092'],
+  clientId: 'example-consumer',
+})
+const producer = kafka.producer();
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',async function(req, res, next) {
   res.render('index', { title: 'Express' });
+  await producer.send({
+    topic: 'prueba',
+    messages: [ 'hola mundo' ]
+  })
 });
 
 module.exports = router;
